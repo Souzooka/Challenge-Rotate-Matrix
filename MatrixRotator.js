ddae70f2b236f6157a69c33a9cd082998866f17e
Dakota <bazookaharwell@gmail.com>
2017-03-23 14:08:49 -1000

    some work

--- MatrixRotator.js
+++ MatrixRotator.js
@@ -1,7 +1,7 @@
 /*  MatrixRotator(matrix)
  *
  *  @param matrix                        a multidimensional array containing the matrix
- *  
+ *
  *  @public property matrix              the matrix
  *
  *  @public method rotate(direction)     direction is either
@@ -13,7 +13,7 @@ var Direction = require("./Direction").Direction;
 
 function MatrixRotator(matrix){
   this.matrix = matrix;
-  
+
 };
 
 //                                         |-- Must be Direction.CW
@@ -21,7 +21,28 @@ function MatrixRotator(matrix){
 MatrixRotator.prototype.rotate = function(direction) {
   // do work here
 
+  let numOfArrays = this.matrix.length;
+  let newMatrix = [];
+  let newArray = [];
+
   // must be a valid Direction, see Direction.js
+  if (direction === Direction.CW) {
+    for (let i = 0; i < numOfArrays; i++) {
+      for (let j = 0; j < numOfArrays; j++) {
+
+      }
+
+      newMatrix.push(newArray);
+    }
+
+
+  }
+  else if (direction === Direction.CCW) {
+
+  } else {
+    return false;
+  }
+
 
 
 };
